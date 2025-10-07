62b0f975e7f5355a6d8a166cb13074eb8a2bd3cd
unknown <guga20.leite@gmail.com>
2025-10-07 00:12:31 -0300

    Finish

---
+++ README.md
@@ -0,0 +1,68 @@
+# Site de personagens da Disney
+
+Este é um site interativo que permite aos usuários explorar o vasto universo de personagens da Disney. Utilizando a [API pública da Disney](https://disneyapi.dev/), a aplicação exibe uma galeria de personagens, permite a busca por nomes específicos e apresenta detalhes sobre cada um.
+
+## Funcionalidades
+
+-   **Tela inicial:** A página inicial carrega uma galeria com todos os personagens disponíveis na API.
+-   **Tela de pesquisa:** Pesquise por qualquer personagem da Disney pelo nome.
+-   **Tela de detalhes do personagem:** Clique em um personagem para ver informações detalhadas, como:
+    -   Filmes
+    -   Curtas-metragens
+    -   Programas de TV
+    -   Jogos
+    -   Aliados e inimigos
+-   **Design Responsivo:** O site se adapta perfeitamente a diferentes tamanhos de tela, como desktops, tablets e celulares.
+
+
+## Design e Layout
+
+O planejamento visual e a prototipagem do projeto foram realizados utilizando as seguintes ferramentas e conceitos:
+
+### Wireframe
+A estrutura básica e o esqueleto de cada página foram definidos nos wireframes, focando na usabilidade e na disposição dos elementos.
+
+-   **[Ver Wireframe da Página Inicial](https://wireframe.cc/BzNHEz)**
+-   **[Ver Wireframe da Página de Pesquisa](https://wireframe.cc/r28Kmb)**
+-   **[Ver Wireframe da Página de Detalhes](https://wireframe.cc/MEFXeg)**
+
+### Design no Figma
+O design de alta fidelidade, incluindo cores, fontes, espaçamentos e a aparência final da interface, foi criado no Figma.
+
+-   **[Ver Design completo no Figma](https://www.figma.com/design/HI3QOUQTyiICA8yjFtkfyX/Projeto-API-Publica?node-id=0-1&t=QRQCTb53v0UgK3X7-1)**
+
+
+## Fotos do site
+
+Aqui abaixo estão algumas imagens do site:
+
+### Tela Inicial (Galeria de Personagens)
+*Nesta tela, todos os personagens são exibidos*
+
+![Tela Inicial do Site](./img/fotoTelaInicial.png)
+
+### Tela de Pesquisa
+*Resultado da busca por um personagem específico*
+
+![Tela de Pesquisa](./img/fotoTelaPesquisa.png)
+
+### Tela de Detalhes do Personagem
+*Aqui são mostradas todas as informações detalhadas de um personagem selecionado*
+
+![Tela de Detalhes](./img/fotoTelaDetalhes.png)
+
+
+## Tecnologias utilizadas para criar o site
+* HTML
+* CSS
+* JS
+* Disney API
+
+
+## API Utilizada
+
+Este projeto consome os dados da **Disney API**, uma API RESTful gratuita com dados sobre personagens da Disney. Link da documentação em: [https://disneyapi.dev/](https://disneyapi.dev/)
+
+
+## Autor
+[Gustavo Pereira](https://www.linkedin.com/in/gustavo-pereira-dev-redes)
--- index.js
+++ index.js
@@ -7,35 +7,53 @@ const containerEsquerda = document.getElementById('container-esquerda')
 const containerDireita = document.getElementById('container-direita')
 
 async function buscarTodasImagens() {
-    const url = `https://api.disneyapi.dev/character`
-    const response = await fetch(url)
-    const dados = await response.json()
-    return dados
+    try {
+        const url = `https://api.disneyapi.dev/character`
+        const response = await fetch(url)
+        const dados = await response.json()
+        return dados
+    } catch (error) {
+        return error
+    }
+
 }
 
 async function criarImagens() {
     containerGeral.textContent = ''
     containerDireita.textContent = ''
     containerEsquerda.textContent = ''
-    const fotos = await buscarTodasImagens()
+    try {
+        const fotos = await buscarTodasImagens()
 
-    fotos.data.forEach(imagem => {
-        const a = document.createElement('a')
-        const img = document.createElement('img')
-        img.src = imagem.imageUrl
-        a.addEventListener('click', function () {
-            exibirDetalhesPersonagem(img.src, fotos)
+        fotos.data.forEach(imagem => {
+            const a = document.createElement('a')
+            const img = document.createElement('img')
+            img.src = imagem.imageUrl
+            a.addEventListener('click', function () {
+                exibirDetalhesPersonagem(img.src, fotos)
+            })
+            a.appendChild(img)
+            containerGeral.appendChild(a)
         })
-        a.appendChild(img)
-        containerGeral.appendChild(a)
-    })
+
+    } catch (error) {
+        const erro = document.createElement('h3')
+        erro.textContent = 'Nenhum personagem encontrado'
+        containerGeral.appendChild(erro)
+    }
+
 }
 
 async function buscarPersonagem(personagem) {
-    const url = `https://api.disneyapi.dev/character?name=${personagem}`
-    const response = await fetch(url)
-    const dados = await response.json()
-    return dados
+    try {
+        const url = `https://api.disneyapi.dev/character?name=${personagem}`
+        const response = await fetch(url)
+        const dados = await response.json()
+        return dados
+    } catch (error) {
+        return error
+    }
+
 }
 
 async function criarImagensPersonagem() {
@@ -43,18 +61,26 @@ async function criarImagensPersonagem() {
     containerDireita.textContent = ''
     containerEsquerda.textContent = ''
     const input = document.getElementById('caixa-de-pesquisa').value
-    const fotos = await buscarPersonagem(input)
 
-    fotos.data.forEach((imagem) => {
-        const a = document.createElement('a')
-        const img = document.createElement('img')
-        img.src = imagem.imageUrl
-        a.addEventListener('click', function () {
-            exibirDetalhesPersonagem(img.src, fotos)
+    try {
+        const fotos = await buscarPersonagem(input)
+
+        fotos.data.forEach((imagem) => {
+            const a = document.createElement('a')
+            const img = document.createElement('img')
+            img.src = imagem.imageUrl
+            a.addEventListener('click', function () {
+                exibirDetalhesPersonagem(img.src, fotos)
+            })
+            a.appendChild(img)
+            containerGeral.appendChild(a)
         })
-        a.appendChild(img)
-        containerGeral.appendChild(a)
-    })
+    } catch (error) {
+        const erro = document.createElement('h3')
+        erro.textContent = 'Personagem não encontrado'
+        containerGeral.appendChild(erro)
+    }
+
 
 }
 
@@ -62,118 +88,125 @@ async function exibirDetalhesPersonagem(urlImage, dados) {
     containerGeral.textContent = ''
     containerDireita.textContent = ''
     containerEsquerda.textContent = ''
-    const informacoes = await dados
-    informacoes.data.forEach(detalhes => {
-        if (detalhes.imageUrl === urlImage) {
-            const h2 = document.createElement('h2')
-            h2.textContent = detalhes.name
+    try {
+        const informacoes = await dados
+        informacoes.data.forEach(detalhes => {
+            if (detalhes.imageUrl === urlImage) {
+                const h2 = document.createElement('h2')
+                h2.textContent = `Personagem "${detalhes.name}"`
 
-            const img = document.createElement('img')
-            img.src = detalhes.imageUrl
-            containerEsquerda.appendChild(h2)
-            containerEsquerda.appendChild(img)
+                const img = document.createElement('img')
+                img.src = detalhes.imageUrl
+                containerEsquerda.appendChild(h2)
+                containerEsquerda.appendChild(img)
 
-            if (detalhes.films.length > 0) {
-                const h3 = document.createElement('h3')
-                h3.textContent = 'Filmes:'
-                containerDireita.appendChild(h3)
-                const ul = document.createElement('ul')
-                detalhes.films.forEach(lista => {
-                    const li = document.createElement('li')
-                    li.textContent = lista
+                if (detalhes.films.length > 0) {
+                    const h3 = document.createElement('h3')
+                    h3.textContent = 'Filmes:'
+                    containerDireita.appendChild(h3)
+                    const ul = document.createElement('ul')
+                    detalhes.films.forEach(lista => {
+                        const li = document.createElement('li')
+                        li.textContent = lista
 
-                    ul.appendChild(li)
-                    containerDireita.appendChild(ul)
-                })
+                        ul.appendChild(li)
+                        containerDireita.appendChild(ul)
+                    })
+                }
+
+                if (detalhes.shortFilms.length > 0) {
+                    const h3 = document.createElement('h3')
+                    h3.textContent = 'Curtas Metragens:'
+                    containerDireita.appendChild(h3)
+                    const ul = document.createElement('ul')
+                    detalhes.shortFilms.forEach(lista => {
+                        const li = document.createElement('li')
+                        li.textContent = lista
+
+                        ul.appendChild(li)
+                        containerDireita.appendChild(ul)
+                    })
+                }
+
+                if (detalhes.tvShows.length > 0) {
+                    const h3 = document.createElement('h3')
+                    h3.textContent = 'Programas de TV:'
+                    containerDireita.appendChild(h3)
+                    const ul = document.createElement('ul')
+                    detalhes.tvShows.forEach(lista => {
+                        const li = document.createElement('li')
+                        li.textContent = lista
+
+                        ul.appendChild(li)
+                        containerDireita.appendChild(ul)
+                    })
+                }
+
+                if (detalhes.videoGames.length > 0) {
+                    const h3 = document.createElement('h3')
+                    h3.textContent = 'Jogos:'
+                    containerDireita.appendChild(h3)
+                    const ul = document.createElement('ul')
+                    detalhes.videoGames.forEach(lista => {
+                        const li = document.createElement('li')
+                        li.textContent = lista
+
+                        ul.appendChild(li)
+                        containerDireita.appendChild(ul)
+                    })
+                }
+
+                if (detalhes.parkAttractions.length > 0) {
+                    const h3 = document.createElement('h3')
+                    h3.textContent = 'Atrações de parque:'
+                    containerDireita.appendChild(h3)
+                    const ul = document.createElement('ul')
+                    detalhes.parkAttractions.forEach(lista => {
+                        const li = document.createElement('li')
+                        li.textContent = lista
+
+                        ul.appendChild(li)
+                        containerDireita.appendChild(ul)
+                    })
+                }
+
+                if (detalhes.allies.length > 0) {
+                    const h3 = document.createElement('h3')
+                    h3.textContent = 'Aliados:'
+                    containerDireita.appendChild(h3)
+                    const ul = document.createElement('ul')
+                    detalhes.allies.forEach(lista => {
+                        const li = document.createElement('li')
+                        li.textContent = lista
+
+                        ul.appendChild(li)
+                        containerDireita.appendChild(ul)
+                    })
+                }
+
+                if (detalhes.enemies.length > 0) {
+                    const h3 = document.createElement('h3')
+                    h3.textContent = 'Inimigos:'
+                    containerDireita.appendChild(h3)
+                    const ul = document.createElement('ul')
+                    detalhes.enemies.forEach(lista => {
+                        const li = document.createElement('li')
+                        li.textContent = lista
+
+                        ul.appendChild(li)
+                        containerDireita.appendChild(ul)
+                    })
+                }
+
+
             }
+        })
 
-            if (detalhes.shortFilms.length > 0) {
-                const h3 = document.createElement('h3')
-                h3.textContent = 'Curtas Metragens:'
-                containerDireita.appendChild(h3)
-                const ul = document.createElement('ul')
-                detalhes.shortFilms.forEach(lista => {
-                    const li = document.createElement('li')
-                    li.textContent = lista
-
-                    ul.appendChild(li)
-                    containerDireita.appendChild(ul)
-                })
-            }
-
-            if (detalhes.tvShows.length > 0) {
-                const h3 = document.createElement('h3')
-                h3.textContent = 'Programas de TV:'
-                containerDireita.appendChild(h3)
-                const ul = document.createElement('ul')
-                detalhes.tvShows.forEach(lista => {
-                    const li = document.createElement('li')
-                    li.textContent = lista
-
-                    ul.appendChild(li)
-                    containerDireita.appendChild(ul)
-                })
-            }
-
-            if (detalhes.videoGames.length > 0) {
-                const h3 = document.createElement('h3')
-                h3.textContent = 'Jogos:'
-                containerDireita.appendChild(h3)
-                const ul = document.createElement('ul')
-                detalhes.videoGames.forEach(lista => {
-                    const li = document.createElement('li')
-                    li.textContent = lista
-
-                    ul.appendChild(li)
-                    containerDireita.appendChild(ul)
-                })
-            }
-
-            if (detalhes.parkAttractions.length > 0) {
-                const h3 = document.createElement('h3')
-                h3.textContent = 'Atrações de parque:'
-                containerDireita.appendChild(h3)
-                const ul = document.createElement('ul')
-                detalhes.parkAttractions.forEach(lista => {
-                    const li = document.createElement('li')
-                    li.textContent = lista
-
-                    ul.appendChild(li)
-                    containerDireita.appendChild(ul)
-                })
-            }
-
-            if (detalhes.allies.length > 0) {
-                const h3 = document.createElement('h3')
-                h3.textContent = 'Aliados:'
-                containerDireita.appendChild(h3)
-                const ul = document.createElement('ul')
-                detalhes.allies.forEach(lista => {
-                    const li = document.createElement('li')
-                    li.textContent = lista
-
-                    ul.appendChild(li)
-                    containerDireita.appendChild(ul)
-                })
-            }
-
-            if (detalhes.enemies.length > 0) {
-                const h3 = document.createElement('h3')
-                h3.textContent = 'Inimigos:'
-                containerDireita.appendChild(h3)
-                const ul = document.createElement('ul')
-                detalhes.enemies.forEach(lista => {
-                    const li = document.createElement('li')
-                    li.textContent = lista
-
-                    ul.appendChild(li)
-                    containerDireita.appendChild(ul)
-                })
-            }
-
-
-        }
-    })
+    } catch (error) {
+        const erro = document.createElement('h3')
+        erro.textContent = 'Personagem não encontrado'
+        containerGeral.appendChild(erro)
+    }
 
 }
 
